fa24517428544f787200a1a0baf54fe66f3c3940
Administrator <Administrator@DESKTOP-CJ2TM4F.mshome.net>
2024-01-05 13:14:56 +0530

    TEST ENG 1

---
+++ demo/src/main/java/demo/TC1.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC1 {
+
+	@Test
+	public void tc1()
+	{
+		Reporter.log(" anil kumar ",true);
+	}
+	
+}
